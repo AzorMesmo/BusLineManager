86040dd7a689bdf9e73d6b923b88216bb1335d75
Azor <mateusazorfrutuoso@gmail.com>
2022-01-19 16:20:10 -0300

    System To Delete Lines

--- BusLineManager.py
+++ BusLineManager.py
@@ -13,6 +13,7 @@ while selection == 0:
     print("4. Add a new schedule")
     print("5. Delete a line")
     print("6. Delete a schedule")
+    print("7. Exit Program\n")
 
     selection = input()
 
@@ -22,9 +23,10 @@ while selection == 0:
         line_list = data.readlines()
         print("")
         for i in line_list:
-            print(i)
-        print("\nType anything to go back")
+            print(i, end='')
+        print("\nType anything to go back\n")
         input()
+        data.close()
         selection = 0
 
     # SEE LINE SCHEDULES
@@ -36,10 +38,10 @@ while selection == 0:
         selection = 1
         while selection == 1:
             # CREATE THE NAME AND ID OF THE LINE AND STORES IN DATA FILE
-            data = open("data.txt", "w")
-            print("\nWrite the line number:")
+            data = open("data.txt", "a")
+            print("\nWrite the line number: ", end='')
             line_number = input()
-            print("Write the line name:")
+            print("Write the line name: ", end='')
             line_name = input()
             data.write(line_number + " " + line_name + "\n")
             print("\nType 1 to add another line and 0 to go back\n")
@@ -47,13 +49,13 @@ while selection == 0:
             if int(selection) == 1:
                 selection = 1
             elif int(selection) == 0:
-                selection = 0
                 data.close()
+                selection = 0
             # IF THE NUMBER TYPED IS INVALID, RETURN TO MENU ANYWAY
             else:
-                print("\nInvalid number typed, returning to menu...")
-                selection = 0
+                print("\nInvalid number typed, returning to menu...\n")
                 data.close()
+                selection = 0
 
     # ADD A NEW SCHEDULE
     elif selection == "4":
@@ -61,13 +63,80 @@ while selection == 0:
 
     # DELETE A LINE
     elif selection == "5":
-        print("5")
+        selection = 1
+        while selection == 1:
+            # SHOW THE LIST OF LINES
+            data = open("data.txt")
+            line_list = data.readlines()
+            print("\nList of Lines\n")
+            for i in line_list:
+                print(i, end='')
+            data.close()
+            # CHOOSE THE LINE
+            data = open("data.txt", "r")
+            print("\nWrite the line number you want to delete: ", end='')
+            line_number = input()
+            deleted_line = -1
+            # VERIFY WITCH LINE IN DATA MUST BE DELETED
+            for i in range(len(line_list)):
+                verify = data.readline(2)
+                print(verify)
+                if verify == line_number:
+                    deleted_line = int(i)
+                next_line = data.readline()  # MAKE THE PROGRAM GO TO NEXT LINE
+            # IF THE LINE WAS NOT FOUND
+            if deleted_line == -1:
+                print("\nThis line was not found")
+                print("\nType 1 to try again ou 0 to go back\n")
+                selection = input()
+                # VERIFY IF THE NUMBER TYPED BY THE USER IS VALID
+                if selection == "1":
+                    selection = 1
+                elif selection == "0":
+                    data.close()
+                    selection = 0
+                else:
+                    print("\nInvalid number typed, returning to menu...\n")
+                    data.close()
+                    selection = 0
+            # IF THE LINE WAS FOUND
+            else:
+                # REMOVE FROM THE LIST OF LINES THE DELETED LINE
+                line_list.pop(deleted_line)
+                print(line_list)
+                data.close()
+                # WIPE ALL CONTENT FROM THE DATA FILE
+                data = open("data.txt", "w")
+                data.write('')
+                data.close()
+                # WRITE THE NEW LIST OF LINES IN DATA FILE
+                data = open("data.txt", "a")
+                print(line_list)
+                for i in range(len(line_list)):
+                    data.write(line_list[i])
+                data.close()
+                print("\nType 1 to delete another line and 0 to go back\n")
+                selection = input()
+                if int(selection) == 1:
+                    selection = 1
+                elif int(selection) == 0:
+                    data.close()
+                    selection = 0
+                # IF THE NUMBER TYPED IS INVALID, RETURN TO MENU ANYWAY
+                else:
+                    print("\nInvalid number typed, returning to menu...\n")
+                    data.close()
+                    selection = 0
 
     # DELETE A SCHEDULE
     elif selection == "6":
         print("6")
 
+    # EXIT PROGRAM
+    elif selection == "7":
+        SystemExit()
+
     # WRONG NUMBER
     else:
-        print("Select a valid option!")
+        print("Select a valid option!\n")
         selection = 0
