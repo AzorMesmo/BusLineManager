4da3a35b57aa05fd10b2d67d8f0adb71e93edfb3
Azor <mateusazorfrutuoso@gmail.com>
2022-01-28 16:30:45 -0300

    Vehicle Interactions Added
    - Vehicles List
    - Schedules Now Ask For Vehile Code
    - Data Storage Changed

--- BusLineManager.py
+++ BusLineManager.py
@@ -23,6 +23,13 @@ def show_lines_list():
     return sll_line_list
 
 
+def get_lines_list():
+    gll_data = open("./data/lines.txt")
+    gll_line_list = gll_data.readlines()
+    gll_data.close()
+    return gll_line_list
+
+
 def binary_selection(bs_selection):
     match bs_selection:
         # RETURNS THE SELECTION FIRST AND THEN THE SUB_SELECTION
@@ -37,13 +44,21 @@ def binary_selection(bs_selection):
 
 selection = 0
 
-# CREATE DATA FILE AND DIRECTORY IF IT DOES NOT EXIST
+# CREATE DATA FILES AND DIRECTORY IF IT DOES NOT EXIST
 
 if not os.path.isdir("./data"):
     os.makedirs("./data")
 
 if not os.path.isfile("./data/lines.txt"):
     data = open("./data/lines.txt", "x")
+    data.close()
+
+if not os.path.isfile("./data/schedules.txt"):
+    data = open("./data/schedules.txt", "x")
+    data.close()
+
+if not os.path.isfile("./data/vehicles.txt"):
+    data = open("./data/vehicles.txt", "x")
     data.close()
 
 # WELCOME MESSAGE
@@ -61,11 +76,13 @@ while selection == 0:
     print("\nType the respective number to navigate\n")
     print("1. See the line list")
     print("2. See the lines schedule")
-    print("3. Add a new line")
-    print("4. Add a new schedule")
-    print("5. Delete a line")
-    print("6. Delete a schedule")
-    print("7. Exit Program\n")
+    print("3. See the vehicle list")
+    print("4. See a vehicle schedule")
+    print("5. Add a new line")
+    print("6. Add a new schedule")
+    print("7. Delete a line")
+    print("8. Delete a schedule")
+    print("9. Exit Program\n")
 
     selection = input()
 
@@ -79,8 +96,22 @@ while selection == 0:
         # SEE LINES SCHEDULE
         case ("2"):
             print("WIP")
-        # ADD A NEW LINE
+        # SEE THE VEHICLE LIST
         case ("3"):
+            data = open("./data/vehicles.txt")
+            vehicle_list = data.readlines()
+            print("\nList of Vehicles\n")
+            for counter in vehicle_list:  # LOOP TO PRINT EACH LINE IN LIST
+                print(counter, end='')
+            data.close()
+            print("\nType anything to go back\n")
+            input()
+            selection = 0
+        # SEE A VEHICLE SCHEDULE
+        case ("4"):
+            print("WIP")
+        # ADD A NEW LINE
+        case ("5"):
             selection = 1
             while selection == 1:
                 # CREATE THE NAME AND ID OF THE LINE AND STORES IN DATA FILE
@@ -88,10 +119,12 @@ while selection == 0:
                 print("\nWrite the line number, (two digits):", end=' ')
                 sub_selection = 1
                 line_number = 0
+                line_list = get_lines_list()
                 while sub_selection == 1:
                     line_number = input()
+                    chosen_line = verify_lines(line_list, line_number)
                     # VERIFY IF THE LINE NUMBER IS VALID
-                    if len(line_number) > 2 or len(line_number) < 2 or int(line_number) <= 0:
+                    if len(line_number) > 2 or len(line_number) < 2 or int(line_number) <= 0 or chosen_line != -1:
                         print("\nInvalid number typed, try again:", end=' ')
                         sub_selection = 1
                     else:
@@ -106,65 +139,91 @@ while selection == 0:
                     selection = input()
                     selection, sub_selection = binary_selection(selection)
         # ADD A NEW SCHEDULE
-        case ("4"):
-            line_list = show_lines_list()
-            print("\nWrite the line number you want to add a schedule:", end=' ')
-            line_number = input()
-            chosen_line = verify_lines(line_list, line_number)
-            if chosen_line == -1:
-                print("\nThis line was not found")
-                print("\nType 1 to try again or 0 to go back:", end=' ')
-                sub_selection = 1
-                while sub_selection == 1:
-                    selection = input()
-                    selection, sub_selection = binary_selection(selection)
-            # IF THE LINE WAS FOUND
-            else:
-                file_name = line_list[chosen_line]
-                file_name = "schedule_line_" + file_name[0:2] + ".txt"
-                # IF THE SCHEDULE OF THE SELECTED LINE WASN'T CREATED, CREATE IT NOW
-                if not os.path.isfile("./data/" + file_name):
-                    data = open("./data/" + file_name, "x")
-                    data.close()
-                data = open("./data/" + file_name, "a")
-                print("\nWrite the hour of the vehicle, (two digits):", end=' ')
-                sub_selection = 1
-                vehicle_hour = "00"
-                while sub_selection == 1:
-                    vehicle_hour = input()
-                    # VERIFY IF THE LINE NUMBER IS VALID
-                    if len(vehicle_hour) > 2 or len(vehicle_hour) < 2 or int(vehicle_hour) > 23 or int(
-                            vehicle_hour) < 0:
-                        print("\nInvalid hour typed, try again:", end=' ')
-                        sub_selection = 1
-                    else:
-                        sub_selection = 0
-                print("\nWrite the minutes of the vehicle, (two digits):", end=' ')
-                sub_selection = 1
-                vehicle_minutes = "00"
-                while sub_selection == 1:
-                    vehicle_minutes = input()
-                    # VERIFY IF THE LINE NUMBER IS VALID
-                    if len(vehicle_minutes) > 2 or len(vehicle_minutes) < 2 or int(vehicle_minutes) > 59 or int(
-                            vehicle_minutes) < 0:
-                        print("\nInvalid minutes typed, try again:", end=' ')
-                        sub_selection = 1
-                    else:
-                        sub_selection = 0
-                print("\nWrite a description, (optional):", end=' ')
-                vehicle_description = input()
-                if vehicle_description != "":
-                    data.write(vehicle_hour + ":" + vehicle_minutes + " " + vehicle_description + "\n")
+        case ("6"):
+            selection = 1
+            while selection == 1:
+                line_list = show_lines_list()
+                print("\nWrite the line number you want to add a schedule:", end=' ')
+                line_number = input()
+                chosen_line = verify_lines(line_list, line_number)
+                if chosen_line == -1:
+                    print("\nThis line was not found")
+                    print("\nType 1 to try again or 0 to go back:", end=' ')
+                    sub_selection = 1
+                    while sub_selection == 1:
+                        selection = input()
+                        selection, sub_selection = binary_selection(selection)
+                # IF THE LINE WAS FOUND
                 else:
-                    data.write(vehicle_hour + ":" + vehicle_minutes + "\n")
-                print("\nType 1 to add another line or 0 to go back\n")
-                data.close()
-                sub_selection = 1
-                while sub_selection == 1:
-                    selection = input()
-                    selection, sub_selection = binary_selection(selection)
+                    data = open("./data/schedules.txt", "a")
+                    print("\nWrite the hour of the vehicle, (two digits):", end=' ')
+                    sub_selection = 1
+                    vehicle_hour = "00"
+                    while sub_selection == 1:
+                        vehicle_hour = input()
+                        # VERIFY IF THE HOUR TYPED IS VALID
+                        if len(vehicle_hour) > 2 or len(vehicle_hour) < 2 or int(vehicle_hour) > 23 or int(
+                                vehicle_hour) < 0:
+                            print("\nInvalid hour typed, try again:", end=' ')
+                            sub_selection = 1
+                        else:
+                            sub_selection = 0
+                    print("\nWrite the minutes of the vehicle, (two digits):", end=' ')
+                    sub_selection = 1
+                    vehicle_minutes = "00"
+                    while sub_selection == 1:
+                        vehicle_minutes = input()
+                        # VERIFY IF THE MINUTES TYPED ARE VALID
+                        if len(vehicle_minutes) > 2 or len(vehicle_minutes) < 2 or int(vehicle_minutes) > 59 or int(
+                                vehicle_minutes) < 0:
+                            print("\nInvalid minutes typed, try again:", end=' ')
+                            sub_selection = 1
+                        else:
+                            sub_selection = 0
+                    print("\nWrite the vehicle code, (five digits):", end=' ')
+                    sub_selection = 1
+                    vehicle_code = "00000"
+                    while sub_selection == 1:
+                        vehicle_code = input()
+                        # VERIFY IF THE VEHICLE CODE IS VALID
+                        if len(vehicle_code) != 5 or not vehicle_code.isdecimal():
+                            print("\nInvalid code typed, try again:", end=' ')
+                            sub_selection = 1
+                        else:
+                            # VERIFY IF THE VEHICLE ALREADY IS IN DATA FILE, IF ISN'T, ADD
+                            data_extra = open("./data/vehicles.txt", "r")
+                            vehicle_list = data_extra.readlines()
+                            data_extra.close()
+                            vehicle_extra = False
+                            data_extra = open("./data/vehicles.txt", "r")
+                            for counter in range(len(vehicle_list)):
+                                vehicle_verify = data_extra.readline(5)  # READ THE 5 FIRST DIGITS OF EACH LINE
+                                if vehicle_verify == vehicle_code:
+                                    vehicle_extra = True
+                                data_extra.readline()  # MAKE THE PROGRAM GO TO NEXT LINE
+                            data_extra.close()
+                            if not vehicle_extra:
+                                data_extra = open("./data/vehicles.txt", "a")
+                                data_extra.write(vehicle_code + "\n")
+                                data_extra.close()
+                                sub_selection = 0
+                            else:
+                                sub_selection = 0
+                    print("\nWrite a description, (optional):", end=' ')
+                    vehicle_description = input()
+                    if vehicle_description != "":
+                        data.write(line_number + " - " + vehicle_code + " - " + vehicle_hour + ":" + vehicle_minutes +
+                                   " " + vehicle_description + "\n")
+                    else:
+                        data.write(line_number + " - " + vehicle_code + " - " + vehicle_hour + ":" + vehicle_minutes + "\n")
+                    print("\nType 1 to add another line or 0 to go back\n")
+                    data.close()
+                    sub_selection = 1
+                    while sub_selection == 1:
+                        selection = input()
+                        selection, sub_selection = binary_selection(selection)
         # DELETE A LINE
-        case ("5"):
+        case ("7"):
             selection = 1
             while selection == 1:
                 line_list = show_lines_list()
@@ -204,10 +263,10 @@ while selection == 0:
                         selection = input()
                         selection, sub_selection = binary_selection(selection)
         # DELETE A SCHEDULE
-        case ("6"):
+        case ("8"):
             print("WIP")
         # EXIT PROGRAM
-        case ("7"):
+        case ("9"):
             SystemExit()
         # INVALID NUMBER
         case _:
